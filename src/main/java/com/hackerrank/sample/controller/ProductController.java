755e925abbc9586e4c099599b5e2c62e4acac1ac
Samuel Modesto <samuelmodestoes@gmail.com>
2026-04-02 15:03:59 -0300

    implementando testes de alertas

--- src/main/java/com/hackerrank/sample/controller/ProductController.java
+++ src/main/java/com/hackerrank/sample/controller/ProductController.java
@@ -5,12 +5,14 @@ import com.hackerrank.sample.service.ProductService;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.tags.Tag;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@Slf4j
 @RestController
 @RequestMapping("/api/v1/products")
 @Tag(name = "Produtos", description = "Endpoints para gerenciamento de produtos")
@@ -25,7 +27,10 @@ class ProductController {
     @Operation(summary = "Obter produto por ID", description = "Retorna os detalhes de um produto específico")
     @ApiResponse(responseCode = "200", description = "Produto encontrado")
     @ApiResponse(responseCode = "404", description = "Produto não encontrado")
-    public ResponseEntity<ProductResponseDto> getProductById(@PathVariable String id) {
+    public ResponseEntity<ProductResponseDto> getProductById(@PathVariable String id) throws InterruptedException {
+
+        log.info("Iniciando busca pelo produto com ID: {}", id);
+
         return ResponseEntity.ok(productService.findProductById(id));
     }
 }
